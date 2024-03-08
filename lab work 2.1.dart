89ad7aa8ee407375b3e18370da6f2baca35e29cc
Dk12211221 <darshankapdi7@gmail.com>
2024-03-08 08:22:23 +0530

    LabWork 2.1

---
+++ lab work 2.1.dart
@@ -0,0 +1,23 @@
+//listview.builder use for list making
+//task 2
+
+
+void main() {
+  performaddition();
+  printhello();
+}
+// Write a Dart program to perform addition of two different numbers.
+performaddition(){
+  int num1 = 10;
+  int num2 = 20;
+
+  int sum = num1 + num2;
+
+  print('The sum of $num1 and $num2 is $sum');
+}
+
+//Write a Dart program to Print Hello Dart.
+printhello(){
+  print('Hello Dart');
+}
+
