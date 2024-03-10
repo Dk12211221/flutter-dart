17371460e9235a0fda5076010e4366f1b08dab75
Dk12211221 <darshankapdi7@gmail.com>
2024-03-10 17:44:39 +0530

    creat oops architecture

---
+++ oops architecture.dart
@@ -0,0 +1,20 @@
+//                1. WAP to take a list using named parameter and return sum of all elements of list.
+
+
+// void main() {
+//   List<int> numbers = [1, 2, 3, 4, 5];
+//   int sum = calculateSum(numbers: numbers);
+//   print('Sum of all elements: $sum');
+// }
+//
+// int calculateSum({required List<int> numbers}) {
+//   int sum = 0;
+//   for (int number in numbers) {
+//     sum += number;
+//   }
+//   return sum;
+// }
+
+
+//                   2. WAP to simulate Super Market Billing system in which..
+
