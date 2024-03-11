8eed8cf514124b63c21298130c91e43e0a80470b
Dk12211221 <darshankapdi7@gmail.com>
2024-03-11 23:38:59 +0530

    create 8.1 and 8.2 projects

---
+++ lab work 8.1.dart
@@ -0,0 +1,158 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(AudiLogoApp());
+// }
+//
+// class AudiLogoApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Audi Logo'),
+//         ),
+//         body: Center(
+//           child: Container(
+//             width: 200,
+//             height: 200,
+//             decoration: BoxDecoration(
+//               border: Border.all(color: Colors.black, width: 2),
+//               borderRadius: BorderRadius.circular(8),
+//             ),
+//             child: Align(
+//               alignment: Alignment.center,
+//               child: Text(
+//                 'AUDI',
+//                 style: TextStyle(
+//                   fontSize: 40,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.red,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//2.
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter Demo'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Container(
+//               width: 200,
+//               height: 200,
+//               decoration: BoxDecoration(
+//                 color: Colors.blue,
+//                 shape: BoxShape.circle,
+//               ),
+//               child: Icon(
+//                 Icons.person,
+//                 color: Colors.white,
+//                 size: 100,
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             Text(
+//               'John Doe',
+//               style: TextStyle(
+//                 fontSize: 24,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(height: 10),
+//             Text(
+//               'john.doe@example.com',
+//               style: TextStyle(
+//                 fontSize: 18,
+//                 color: Colors.grey,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//3.
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(AudiLogoApp());
+// }
+//
+// class AudiLogoApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Audi Logo'),
+//         ),
+//         body: Center(
+//           child: Container(
+//             width: 200,
+//             height: 200,
+//             decoration: BoxDecoration(
+//               border: Border.all(color: Colors.black, width: 2),
+//               borderRadius: BorderRadius.circular(8),
+//             ),
+//             child: Align(
+//               alignment: Alignment.center,
+//               child: Text(
+//                 'Audi',
+//                 style: TextStyle(
+//                   fontSize: 40,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.red,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+//task 2
+
